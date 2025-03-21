8bc15a01bdbb8ca0108ba1b852ee6e1411b8f568
hhub25 <hetvi853@gmail.com>
2025-03-21 01:28:42 -0400

    Bypassing Husky

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { provideStore } from "@ngrx/store";
 import { provideEffects } from "@ngrx/effects";
 import { eventReducer } from "./store/reducers/event.reducer";
 import { EventEffects } from "./store/effects/event.effects";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"; 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore({ events: eventReducer }), provideEffects([EventEffects])],
+  providers: [provideRouter(routes), provideStore({ events: eventReducer }), provideEffects([EventEffects]), importProvidersFrom(BrowserAnimationsModule),],
 };
